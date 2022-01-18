48324e97d14b286b24bb2cfe4c07cc9b4d2da484
alyshapm <91533472+alyshapm@users.noreply.github.com>
2022-01-18 15:47:02 +0700

    Alysha's updates

---
+++ myscripts.js
@@ -0,0 +1,22 @@
+const registerBtn = document.querySelector(".register-btn");
+const sendBtn = document.querySelector(".send-btn");
+const popupBox = document.querySelector(".popup-overlay");
+const closeBtn = document.querySelector(".close")
+
+registerBtn.addEventListener("click",() => {
+    popupBox.classList.add("active")
+})
+
+sendBtn.addEventListener("click",() => {
+    popupBox.classList.remove("active")
+})
+
+closeBtn.addEventListener("click",() => {
+    popupBox.classList.remove("active")
+})
+
+window.onclick = function(event) {
+    if (event.target == popupBox) {
+        popupBox.classList.remove("active")
+    }
+}
