18db4084959c97fda6242167379713fc2f8423e4
alyshapm <91533472+alyshapm@users.noreply.github.com>
2022-01-18 20:42:07 +0700

    Alysha's updates
    Renaming files, adding new pages

--- myscripts.js
+++ myscripts.js
@@ -4,19 +4,19 @@ const popupBox = document.querySelector(".popup-overlay");
 const closeBtn = document.querySelector(".close")
 
 registerBtn.addEventListener("click",() => {
-    popupBox.classList.add("active")
-})
+    popupBox.classList.add("active");
+});
 
 sendBtn.addEventListener("click",() => {
-    popupBox.classList.remove("active")
-})
+    popupBox.classList.remove("active");
+});
 
 closeBtn.addEventListener("click",() => {
-    popupBox.classList.remove("active")
-})
+    popupBox.classList.remove("active");
+});
 
 window.onclick = function(event) {
     if (event.target == popupBox) {
-        popupBox.classList.remove("active")
+        popupBox.classList.remove("active");
     }
 }
