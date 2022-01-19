07bb3415f5d50ec778f8fd02ca1a96138cf48181
alyshapm <91533472+alyshapm@users.noreply.github.com>
2022-01-19 14:55:05 +0700

    Alysha's updates

--- myscripts2.js
+++ myscripts2.js
@@ -2,12 +2,6 @@ const sideProfile = document.querySelector(".profile-container");
 const profToggle = document.querySelector(".profile");
 const closeBtn = document.querySelector(".close");
 
-// profToggle.addEventListener("click",() => {
-//     const visibility = sideProfile.getAttribute("data-visible");
-
-//     console.log(visibility);
-// })
-
 profToggle.addEventListener("click",() => {
     sideProfile.classList.add("active")
 });
